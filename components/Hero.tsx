5c86ecb209404c1a20100824540da999098a48db
Jev <jev@iriepixel.com>
2022-05-25 11:42:06 +0100

    IRIEPIXEL space removed from the glitch

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,26 +4,26 @@ import Image from "next/image";
 import styles from "../styles/Hero.module.scss";
 
 export const Hero = () => {
- return (
-  // <div className={styles.hero} style={backgroundColor && { backgroundColor }} {...props}>
-  // 	<div className={styles.logo}>
-  // 		<h1 className={styles.title}>{title}</h1>
-  // 		<Image src='/logo-iriepixel.svg' alt='IRIE PIXEL Logo' width={137} height={79} priority />
-  // 	</div>
-  // </div>
-  <>
-   <div className={styles.container}>
-    <div className={styles.glitch} data-text="IRIEPIXEL">
-     IRIE PIXEL
-    </div>
-    <div className={styles.glow}>IRIE PIXEL</div>
-    <p className={styles.subtitle}>
-     <a href="mailto:jev@iriepixel.com">jev@iriepixel.com</a>
-    </p>
-   </div>
-   {/* <div className={styles.scanlines}></div> */}
-  </>
- );
+  return (
+    // <div className={styles.hero} style={backgroundColor && { backgroundColor }} {...props}>
+    // 	<div className={styles.logo}>
+    // 		<h1 className={styles.title}>{title}</h1>
+    // 		<Image src='/logo-iriepixel.svg' alt='IRIE PIXEL Logo' width={137} height={79} priority />
+    // 	</div>
+    // </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.glitch} data-text="IRIEPIXEL">
+          IRIEPIXEL
+        </div>
+        <div className={styles.glow}>IRIEPIXEL</div>
+        <p className={styles.subtitle}>
+          <a href="mailto:jev@iriepixel.com">jev@iriepixel.com</a>
+        </p>
+      </div>
+      {/* <div className={styles.scanlines}></div> */}
+    </>
+  );
 };
 
 // Hero.propTypes = {
