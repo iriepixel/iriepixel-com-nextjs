54f4b89cd40ad756fcd0186a2314f2e69ced4242
Jev <jev@iriepixel.com>
2022-05-25 11:19:13 +0100

    desc updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>IRIE PIXEL - JAMstack Developer</title>
-        <meta name="description" content="JAMstack front end web developer with over 10 years of experience, based in Brighton / London -> remote" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <title>IRIE PIXEL - JAMstack Front End Web Developer</title>
+        <meta name="description" content="JAMstack front end freelance / contract web developer with over 10 years of experience, based in Brighton / London / Remote - Javascript, React, Vue, Next, Nuxt." />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
+        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> */}
         {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" /> */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
